82ca281329c15240e113f328e30053e397906b8d
BuyAnyCoin <buyanycoin@gmail.com>
2015-04-28 20:25:02 -0800

    Updated password disable text

--- client/views/authentication/login.js
+++ client/views/authentication/login.js
@@ -19,7 +19,7 @@ Template.Login.events({
         // could be incorrect. Inform the user that their
         // login attempt has failed.      
         if (err.reason.indexOf('forbidden') > -1) {
-          toastr.error("Your account is not active. Please contact admin to activate your account.", 'Login error: Account blocked');
+          toastr.error("Your account is temporarily disable. Please wait or reset your password.", 'Login error: Account Locked');
         }
         else{
           toastr.error(err.reason, 'Login error');
