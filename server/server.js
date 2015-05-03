76a1ca0d0b8993d57a6ee4d2dd26558a97206cae
ansmoh <anshuman.ewest@gmail.com>
2015-05-03 21:16:55 +0530

    update
    Fix:
    
    1.  When the user has a USD balance and buys a coin the transaction
    should not go to pending status but rather to a complete status.
    2.  Inside the coin modal on the bottom, once there are transaction the
    table is not being contained within the boarders of the modal.
    3.  In the buy modal the text "By clicking Buy you agree to the terms
    and conditions" needs to be aligned left.
    4. In the buy modal the Total Balance needs to say "USD Balance:"
    5. Fixed a Bug in Voucher Coupon

--- server/server.js
+++ server/server.js
@@ -90,7 +90,11 @@ var Utility = {
     check(currency, String);
     check(amount, Number);
     check(note, String);
-
+    console.log(currency);
+    var status = "pending";
+    if(currency === 'USD'){
+      status = "complete";
+    }
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-logged-in", "Must be logged in to initiate a transaction.");
     }
@@ -99,7 +103,7 @@ var Utility = {
         currency: currency,
         amount: amount,
         note: note,
-        status: 'pending',
+        status: status,
         timestamp: Date()
       };
     if( txnid !== undefined ){
@@ -216,12 +220,19 @@ var Utility = {
     if(user){
       if(_.has(user, "redeem_info") && user.redeem_info.length >= 10){
         var dates = user.redeem_info;
-        var lastTenthDate = dates.slice(Math.max(dates.length - 10, 1));
-        var diffMs = (d - lastTenthDate[0]);
-        var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
-        if(diffMins <= 30){
-          throw new Meteor.Error("You can redeem only 10 vouchers in 30 minutes");
+        console.log(dates);
+        if(dates.length >= 10){
+          var lastTenthDate = dates.length - 10;
+          var diffMs = (d - dates[lastTenthDate]);
+          var diffMins = Math.round(diffMs  / 60000);
+          if(diffMins <= 30){
+            throw new Meteor.Error("You can redeem only 10 vouchers in 30 minutes");
+          }
         }
+        else{
+          AccountStatus.update({userId: this.userId}, {$push: {redeem_info: d}});
+        }
+        
       }
       else{
         
