bc28e9e61c0ec4594a8751839307e134fae1ded7
ansmoh <anshuman.ewest@gmail.com>
2015-05-05 00:46:56 +0530

    Update
    Fix:
    1. Removed Sikka & related code
    2. In mobile buy & sell are working now
    3. Added active class for progress bar

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ Meteor.startup(function () {
   // code to run on server at startup.
   // to send mails
   // process.env.MAIL_URL = "smtp://donotreply%40buyanycoin.com:njiokmNJIOKM@smtp.gmail.com:587/";
-  process.env.SIKKA_BLOCK_IP_FOR_MILLIS = 1140000;
+  // process.env.SIKKA_BLOCK_IP_FOR_MILLIS = 1140000;
   Accounts.emailTemplates.siteName = "BuyAnyCoin";
   Accounts.emailTemplates.from = "BuyAnyCoin <donotreply@buyanycoin.com>";
   Accounts.emailTemplates.resetPassword.text = function(user, url) {
