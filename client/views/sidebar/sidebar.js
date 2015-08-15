c09ebee9559d482151cb3da6c69e85cb1709cb09
Anshuman <anshuman.ewest@gmail.com>
2015-08-15 17:06:41 +0530

    Update
    Update.....

--- client/views/sidebar/sidebar.js
+++
@@ -1,9 +0,0 @@
-Template.SideBar.events({
-  'click .off-screen a': function (e, t) {
-    $("div.off-screen").removeClass("off-screen");
-    $(".side-nav").removeClass('dk');
-  },
-  'click .signout':function (e) {
-    Meteor.logout()
-  }
-});
